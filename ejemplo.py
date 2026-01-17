fdb0b965a79820c11727b5127f07809c3a2c5b35
Cuauilp <cuauhtemoc.martinez.b@gmail.com>
2026-01-17 11:50:19 -0600

    Primer archivo

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+#COMENTARIOS
+#Son una descripcion del programa o codigo
+#A los comentarios los omite o no los toma en cuenta Python
+
+#Ejemplo
+print("Nombre:cuauhtemoc")     # Nombre
+print('Primer Apellido: martinez')       # Primer apellido
+print('Segundo Apellido: bolaños');       # Segundo apellido
+print() # Linea de codigo en blanco
+print("Suma =", 5+2)    # Suma
+print("Resta =", 10-2)    # Resta
+print("Mult = ", 3*4)     # Multiplicacion
+print("Division =", 8/2)  # Division
